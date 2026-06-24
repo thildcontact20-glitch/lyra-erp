2e7b02f8ba5fc508ce4b7276eb590c135408c530
Mystic Mythos <mystic@vivalyscompagny.com>
2026-06-24 11:13:43 +0200

    fix: remove standalone output, remove custom Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: { serverComponentsExternalPackages: ['bcryptjs', 'prisma', '@prisma/client'] },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  skipTrailingSlashRedirect: true,
 }
 module.exports = nextConfig
